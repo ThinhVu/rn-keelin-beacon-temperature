958093f2a41764513becee085048dea17f3ca5b4
ThinhVu <vutrongthinhk7@gmail.com>
2020-12-09 17:34:48 +0700

    Correct typing Downgrade to rn 0.62

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { GigasourceKeelinTemperatureBeacon: NI } = NativeModules;
 export default {
   getInfo: async (macAddress) => {
     // see Android GigasourceKeelinTemperatureBeaconModule
-    if (Platform.os !== 'android') {
-      console.log(`Gathering ble info from ${Platform.os} is not supported.`);
+    if (Platform.OS !== 'android') {
+      console.log(`Gathering ble info from ${Platform.OS} is not supported.`);
       return null;
     }
 
